1d3db46ce0284b1794ef824103fde98acf5af3e2
Kevin Kelly <ircmullaney@gmail.com>
2014-03-25 18:37:48 -0500

    Remove comments from user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,33 +1,6 @@
 ActiveAdmin.register User do
 
-    # t.string   "email",                  default: "", null: false
-    # t.string   "encrypted_password",     default: "", null: false
-    # t.string   "reset_password_token"
-    # t.datetime "reset_password_sent_at"
-    # t.datetime "remember_created_at"
-    # t.integer  "sign_in_count",          default: 0,  null: false
-    # t.datetime "current_sign_in_at"
-    # t.datetime "last_sign_in_at"
-    # t.string   "current_sign_in_ip"
-    # t.string   "last_sign_in_ip"
-    # t.datetime "created_at"
-    # t.datetime "updated_at"
-
   permit_params :email
 
   filter :email
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
 end
